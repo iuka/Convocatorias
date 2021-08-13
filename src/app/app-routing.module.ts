1676e63b1d9e96de504f39cd929cde12b795db1e
iuka <roberto.t37@gmail.com>
2021-08-12 20:01:15 -0500

    rt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { ConvocatoriaunoComponent } from './convocatoriauno/convocatoriauno.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'convocatoriauno', component: ConvocatoriaunoComponent },
   { path: '**', component: HomeComponent }
   
 
